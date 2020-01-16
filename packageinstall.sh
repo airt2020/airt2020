3dc12838367bb2c68acb771f72a938b1a49ed43f
Norifumi Irie <norifumi.irie@tyrellsys.com.my>
2020-01-16 15:06:48 +0800

    set up packageinstall.sh and provision.sh to successfully install Django Toolbelt

--- packageinstall.sh
+++ packageinstall.sh
@@ -1,6 +1,8 @@
 #Install pip packages
 sudo echo "start installing pip packages..."
+sudo /opt/rh/rh-python36/root/usr/bin/pip install psycopg2-binary
 sudo /opt/rh/rh-python36/root/usr/bin/pip install requests
 sudo /opt/rh/rh-python36/root/usr/bin/pip install Django
-sudo echo "Done!"
-sudo /opt/rh/rh-python36/root/usr/bin/pip freeze >> requirement_tmp.txt
+sudo /opt/rh/rh-python36/root/usr/bin/pip install django-toolbelt
+sudo echo "Pip installation done!"
+sudo /opt/rh/rh-python36/root/usr/bin/pip freeze > requirement_tmp.txt
